4ce8fc723af3acc0f655ff382bdac54ed56fc35c
wgs <wgshuaiit@163.com>
2020-06-11 10:27:16 +0800

    配置swagger2

--- springboot-api/src/main/java/org/example/controller/HelloController.java
+++ springboot-api/src/main/java/org/example/controller/HelloController.java
@@ -1,16 +1,16 @@
 package org.example.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Wgs (ˇˍˇ)
- * @version 1.0
- * @create：2020/06/10
- */
+
+@Api(value = "测试swagger", tags = {"测试的相关接口"})
 @RestController
 public class HelloController {
+    @ApiOperation(value = "获取测试接口", notes = "获取测试结果", httpMethod = "GET")
     @GetMapping("/hello")
     public JSONObject hello() {
         JSONObject jsonObject = new JSONObject();
